f29e91d64a978a9f1025f469242c4dbdcefff88c
Shojib Mahmud <shojibmahmud109@gmail.com>
2024-06-26 17:48:19 +0600

    About page with chakra

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,15 @@
 // src/components/About.tsx
 import React from "react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const About: React.FC = () => {
 	return (
-		<section id="about">
-			<h2>About Me</h2>
-			<p>Write a brief introduction about yourself here.</p>
-		</section>
+		<Box p={4} id="about">
+			<Heading as="h2" size="xl" mb={4}>
+				About Me
+			</Heading>
+			<Text fontSize="lg">Write a brief introduction about yourself here.</Text>
+		</Box>
 	);
 };
 
